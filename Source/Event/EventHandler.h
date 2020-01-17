84ed0df6dba888ce42ca8b411d541df38438d4fc
Jin <bomjan@naver.com>
2020-01-17 17:24:19 -0500

    Implemented event handler

--- Source/Event/EventHandler.h
+++ Source/Event/EventHandler.h
@@ -1,5 +1,11 @@
 #pragma once
+
+template <class T>
 class EventHandler
 {
+private : 
+
+public : 
+	virtual void handle(T* e) = 0;
 };
 
--- Source/main.cpp
+++ Source/main.cpp
@@ -1,10 +1,27 @@
 #include "Application.h"
 #include <iostream>
+#include "Event/InputEvent.h"
+#include "Event/KeyEvent.h"
+#include "Event/MouseEvent.h"
+#include "Event/EventHandler.h"
 
 LPCWSTR title = TEXT("tile-game");
 
 #ifdef _DEBUG
 #include "Debug/Debug.h"
+
+class Scene
+{
+public:
+	EventHandler<MouseEvent>* onMousePressed;
+	void SetOnMousePressed(EventHandler<MouseEvent>* me)
+	{
+		onMousePressed = me;
+	}
+};
+
+
+
 int main()
 {
 	AllocConsole();
@@ -16,6 +33,33 @@ int main()
 		MessageBox(app.GetHWND(), TEXT("Creating window failed"), TEXT("Window Error"), 0);
 		return -1;
 	}
+
+	struct A : EventHandler<KeyEvent>
+	{
+		void handle(KeyEvent* e) override
+		{
+			Debug::Log("Custom EventHandler<KeyEvent>");
+		}
+	}_A;
+	KeyEvent ke;
+	_A.handle(&ke);
+
+	
+
+	Scene scene1;
+	scene1.SetOnMousePressed([]()->EventHandler<MouseEvent>*
+	{
+		struct B : EventHandler<MouseEvent>
+		{
+			void handle(MouseEvent* e) override
+			{
+				Debug::Log("Custom EventHandler From ANONYMOUS FUNCTION Lambda<MouseEvent>");
+			}
+		};
+		return new B;
+	}());
+	MouseEvent me;
+	scene1.onMousePressed->handle(&me);
 
 	return static_cast<int>(app.Run());
 }
