56a51562ab862bcacab6af0890b30dc8c799edbe
Jin <bomjan@naver.com>
2020-01-28 10:00:41 -0500

    Implementing input

--- Source/Input/Input.h
+++ Source/Input/Input.h
@@ -1,5 +1,31 @@
 #pragma once
+#include <vector>
+#include "KeyCode.h"
+
+
 class Input
 {
-};
+private :
+	std::vector<KeyCode> pressed;
+	std::vector<KeyCode> hold;
+	std::vector<KeyCode> released;
 
+	static Input* instance;
+
+public :
+	static Input* Instance();
+	static void NextFrame();
+	static void KeyPressed(KeyCode key);
+	static void KeyReleased(KeyCode key);
+
+	static bool GetKeyDown(KeyCode key);
+	static bool GetKeyUp(KeyCode key);
+	static bool GetKey(KeyCode key);
+
+	static bool GetMouseButtonDown(KeyCode button);
+	static bool GetMouseButtonUp(KeyCode button);
+	static bool GetMouseButton(KeyCode button);
+
+	static unsigned short GetMouseX();
+	static unsigned short GetMouseY();
+};
--- Source/Input/RawInput.cpp
+++ Source/Input/RawInput.cpp
@@ -1,5 +1,6 @@
 #include <windowsx.h>
 #include "RawInput.h"
+#include "Input.h"
 #include "../Event/EventManager.h"
 #include "../Event/MouseEvent.h"
 #include "../Event/KeyEvent.h"
@@ -35,11 +36,26 @@ LRESULT RawInput::WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 	case WM_LBUTTONDOWN:
 		SetMouseData(_mData, hWnd, wParam, lParam);
 		_mData.button = KeyCode::Mouse0;
+		Input::KeyPressed(KeyCode::Mouse0);
 		EventManager::Instance()->RegisterEvent(_mData.type, &_mData);
+		break;
+	case WM_LBUTTONUP:
+		Input::KeyReleased(KeyCode::Mouse0);
+		break;
+	case WM_RBUTTONUP:
+		Input::KeyReleased(KeyCode::Mouse1);
+		break;
+	case WM_RBUTTONDOWN:
+		Input::KeyPressed(KeyCode::Mouse1);
 		break;
 	case WM_KEYDOWN:
 		SetKeyData(_kData, hWnd, wParam, lParam);
+		Input::KeyPressed(_kData.key);
 		EventManager::Instance()->RegisterEvent(_kData.type, &_kData);
+		break;
+	case WM_KEYUP:
+		SetKeyData(_kData, hWnd, wParam, lParam);
+		Input::KeyReleased(_kData.key);
 		break;
 	case WM_CLOSE:
 		PostQuitMessage(0);
